25b7242551f004c28dc4f46f55fcce798262f3c1
Rashid Razak <bib.nexus@gmail.com>
2019-08-11 05:43:55 +0800

    Use Expanded widget to create flexible layouts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter Dice'),
-          ),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red[900],
+        appBar: AppBar(
+          title: Text('Flutter Dice'),
+          backgroundColor: Colors.red[900],
         ),
+        body: DicePage(),
       ),
-    );
+    ),
+  );
+}
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice2.png'),
+        ),
+      ],
+    );
   }
 }
